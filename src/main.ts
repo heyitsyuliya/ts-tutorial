c6c19dc9a0fac2bc9657c0cc6033b31804e0b82f
Yuliya Kandratsyeva <ykandratsyeva@gmail.com>
2021-04-22 16:30:58 -0400

    adding notes for using typescript with dom elements

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,22 @@ interface UserInterface {
 }
 
 let username: string = 'yuliya';
-
 let user: UserInterface | null =  null;
+
+
+// USING TYPESCRIPT WITH DOM ELEMENTS
+// When dealing with HTML elements in typescript, it doesn't know how to deal with the markup, TS simply skips it
+// That's why we need to specify the exact types of elements we're dealing with
+
+// Element is the highest class in the hierarchy
+const profileCard = document.querySelector('.main')
+
+// this will throw an error that property value does not exist on Element
+//console.log('profile card', profileCard.value)
+
+// to fix the issue, said element needs to be converted into a more specific type like this
+const htmlElement = document.querySelector('.profile') as HTMLInputElement;
+// and now that TypeScript knows what exact kind of element it's dealing with
+// we have access to all html element properties
+console.log('new html element', htmlElement.value)
+
